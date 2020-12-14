6841eb2bcf8a2d8034fdc9bef8b785c049de5c78
root <root@ip-172-31-24-72.ec2.internal>
2020-12-13 19:52:27 -0500

    example of keys

---
+++ keys.py
@@ -0,0 +1,6 @@
+db = "gearBot" #database name
+gearBot = '' #discord bot api key
+invite_discord = 'https://discord.gg/zpjvZj7jAG' #ignore
+invite_bot = 'https://discord.com/oauth2/authorize?client_id=344643767313235968&scope=bot&permissions=0' #ignore 
+fourofour = 'why' #ignore
+announcement = 'Migrated to a new database left the old data behind, sorry. jay#8008 with any bugs or concerns.' #ignore
